b219f40671011ba17b263db209d7fd610d6f7fd4
rioscris <rios7563@gmail.com>
2019-09-16 03:41:14 -0300

    All comments

--- ejercicio3/sources/programa.c
+++ ejercicio3/sources/programa.c
@@ -1,8 +1,19 @@
-printf("hola");
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	/*
+	/*
+	/*
+	 *
+	 */
+	                            /**/
+	printf("Hola Mundo!\n");
+	printf("hola otra vez\n"); // comentario inline luego de codigo
+}
 // comentario
 // Otro /////////////////////////////////////////////////////comentario
 // Doble comentario // lineal
 /* Comentario lineal */
 /* Comentario bilineal
  * Y aca termina*/
-printf("chau");
