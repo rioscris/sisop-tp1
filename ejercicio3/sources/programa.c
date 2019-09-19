ec8af4cfd2f9dd1902348cb66b1355e91501a8f3
Cristian Rios <cristian.rios@moxit.net>
2019-09-19 03:50:26 -0300

    Fixing Merge

--- ejercicio3/sources/programa.c
+++ ejercicio3/sources/programa.c
@@ -22,27 +22,3 @@ int main(){
 /* Comentario lineal */
 /* Comentario bilineal
  * Y aca termina*/
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-	/*
-	/*
-	/*
-	 *
-	 
-	 */
-	                            /**/
-	printf("Hola Mundo!\n");
-	printf/*comentario bloque dentro de linea*/("Linea de codigo con comentario en el medio\n")/*comentario bloque dentro de linea*/;
-	printf("Linea de codigo con comentario al final\n"); // comentario inline luego de codigo
-	printf("String con doble barra // en el medio\n");
-	printf("Linea de codigo con doble barra al final\n");//
-	printf("Linea de codigo con una sola barra al final\n");/
-}
-// comentario
-// Otro /////////////////////////////////////////////////////comentario
-// Doble comentario // lineal
-/* Comentario lineal */
-/* Comentario bilineal
- * Y aca termina*/
